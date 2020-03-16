dd42b6a23b8d5f0f63fb36abca351465601f4e30
missukevitch <missukevitch@gmail.com>
2020-03-16 20:44:05 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+    'ten', 'eleven', 'twelve','thirteen','fourteen','fifteen','sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    
+    if(number < 20)
+        return numbers[number];
+
+    const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    if(number < 100) {
+        if(number % 10 === 0) 
+            return tens[number/10];
+        else return tens[Math.floor(number/10)] + ' ' + numbers[number%10];
+    }
+    if(number % 100 === 0) {
+        return numbers[number/100] + ' hundred';
+    }
+    if(number%10 === 0) {
+        return numbers[Math.floor(number/100)] + ' hundred ' + tens[number%100/10];
+    }
+    if(number%100 < 20)
+        return numbers[Math.floor(number/100)] + ' hundred ' + numbers[number%100];
+    return numbers[Math.floor(number/100)] + ' hundred ' + tens[Math.floor(number%100/10)] + ' ' + numbers[Math.floor(number%10)];
 }
